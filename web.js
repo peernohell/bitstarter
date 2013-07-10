7c3010e63d2684f2f045d74dd01e761aa257bddd
Francois Lecroart <peernohell@gmail.com>
2013-07-10 19:45:11 +0000

    Add index.html and update web.js to use it

--- web.js
+++ web.js
@@ -1,12 +1,27 @@
-var express = require('express');
+var
+fs = require('fs'),
+express = require('express'),
+app = {
+	buffer: null,
+	port: process.env.PORT || 5000,
+	express: express.createServer(express.logger()),
+	onIndexRead: function (err, data) {
+		if (err) {
+			console.log("can't read index file!");
+			console.log(err);
+			return;
+		}
+		this.buffer = data.toString();
+		this.startServer();
+	},
+	onGetRoot: function(request, response) {
+	  response.send(this.buffer);
+	},
+	startServer: function () {
+		this.express.get('/', this.onGetRoot.bind(this));
+		this.express.listen(this.port, console.log.bind(console, "Listening on " + this.port));
+	}
+};
 
-var app = express.createServer(express.logger());
+fs.readFile('index.html', app.onIndexRead.bind(app));
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});

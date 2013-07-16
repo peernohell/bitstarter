2db4e93722d8100ef31146abb0049594953abe8f
peernohell <peernohell@gmail.com>
2013-07-16 17:47:22 +0200

    Replace Ubuntu and OpenSans font by Fjord One. add margin on first pic
    change font and update web.js to update index.html when page change

--- web.js
+++ web.js
@@ -2,26 +2,37 @@ var
 fs = require('fs'),
 express = require('express'),
 app = {
-	buffer: null,
+	indexHtml: "Loading ...",
 	port: process.env.PORT || 5000,
 	express: express.createServer(express.logger()),
 	onIndexRead: function (err, data) {
 		if (err) {
 			console.log("can't read index file!");
 			console.log(err);
+			this.indexHtml = err.toString();
 			return;
 		}
-		this.buffer = data.toString();
-		this.startServer();
+		console.log("index file OK!");
+		this.indexHtml = data.toString();
+	},
+	readIndex: function () {
+		this.indexHtml = "<h1>Page not yet loaded! comme back later</h1>";
+		fs.readFile('index.html', this.onIndexRead.bind(this));
 	},
 	onGetRoot: function(request, response) {
-	  response.send(this.buffer);
+	  response.send(this.indexHtml);
 	},
-	startServer: function () {
+	start: function () {
+		// read index.html
+		this.readIndex();
+
+		// recall readIndex if file change
+		fs.watchFile('index.html', this.readIndex.bind(this));
+
+		// start http server
 		this.express.get('/', this.onGetRoot.bind(this));
 		this.express.listen(this.port, console.log.bind(console, "Listening on " + this.port));
 	}
 };
 
-fs.readFile('index.html', app.onIndexRead.bind(app));
-
+app.start();
